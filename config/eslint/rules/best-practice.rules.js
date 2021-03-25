5e98d0f9f22cf282a287b0160772be59310e9d58
Stanislav Halyn <41181938+stanislav-halyn@users.noreply.github.com>
2021-03-25 14:03:35 +0200

    Feature/sql builder (#3)
    * build: disbale an eslint rule
    
    * feat: add `sql-builder` utils

--- config/eslint/rules/best-practice.rules.js
+++ config/eslint/rules/best-practice.rules.js
@@ -7,13 +7,13 @@ module.exports = {
     'lines-between-class-members': 'off',
     'no-else-return': ['error', { allowElseIf: true }],
     'arrow-parens': ['error', 'as-needed'],
-    'newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+    'newline-per-chained-call': 'off',
     'no-param-reassign': ['error', { props: false }],
     'no-underscore-dangle': 'off',
     'operator-linebreak': ['error', 'before'],
     'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
     'no-inline-comments': 'off',
     'no-return-await': 'error',
-    'no-trailing-spaces': 'error'
-  }
+    'no-trailing-spaces': 'error',
+  },
 };
